fb9480b66ebe685b0ffe46cdc2fe1215f6b8127c
humblegod67 <151152304+humblegod67@users.noreply.github.com>
2023-11-20 11:04:49 +0300

    Add files via upload

---
+++ задача 1(20.11).cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int n;
+	int t;
+	int result;
+	cout << "Введите первое число: ";
+	cin >> n;
+	cout << "Введите второе число: ";
+	cin >> t;
+	cout << "Результат умножения этих двух чисел?";
+	cin >> result;
+	if (result == n * t)
+	{
+		cout << "Правильно ";
+	}
+	else
+	{
+
+		cout << "неправильно  "<<(n*t);
+		
+	}
+	return 0;
+}
+
+
---
+++ задача 2 20.11.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int day;
+	cout << "Введите день недели по счету ";
+		cin >> day;
+	if ((day == 1) || (day == 2))
+		cout << " Отдыхаю" << endl;
+	else if ((day == 3) || (day == 4) || (day == 5) || (day == 6) || (day == 7))
+		cout << " учусь" << endl;
+
+}
+
+
